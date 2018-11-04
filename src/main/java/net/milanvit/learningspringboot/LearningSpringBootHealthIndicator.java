251d110ad938093df8d124cbb958b93d25aa4539
Milan Vit <milanvit@milanvit.net>
2018-11-04 14:16:53 +0900

    Added more logging and monitoring

---
+++ src/main/java/net/milanvit/learningspringboot/LearningSpringBootHealthIndicator.java
@@ -0,0 +1,31 @@
+package net.milanvit.learningspringboot;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@Component
+public class LearningSpringBootHealthIndicator implements HealthIndicator {
+    @Override
+    public Health health() {
+        try {
+            URL url = new URL("https://www.milanvit.net");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            int statusCode = conn.getResponseCode();
+
+            if (statusCode >= 200 && statusCode < 300) {
+                return Health.up().build();
+            } else {
+                return Health.down()
+                    .withDetail("HTTP Status Code", statusCode)
+                    .build();
+            }
+        } catch (IOException e) {
+            return Health.down(e).build();
+        }
+    }
+}
